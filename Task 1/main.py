8a4561aa260d82908babade36e664daa80417760
mik00laj <mikolaj.bania@gmail.com>
2024-03-03 23:36:34 +0100

    napisać sprawozdanie

--- Task 1/main.py
+++ Task 1/main.py
@@ -15,64 +15,36 @@ def gradient_descent(initial_x, initial_y, learning_rate, num_iterations, tolera
     x_history = [x]
     y_history = [y]
 
-
     for i in range(num_iterations):
         d = gradient(x, y)
-        diff_x = - learning_rate * d[0]
-        diff_y = - learning_rate * d[1]
-        x = x + diff_x
-        y = y + diff_y
+        x = x - learning_rate * d[0]
+        y = y - learning_rate * d[1]
         x_history.append(x)
         y_history.append(y)
         if i > 0 and abs(f(x_history[i], y_history[i]) - f(x_history[i-1], y_history[i-1])) <= tolerance:
             break
 
-
-
-
     return x, x_history, y, y_history
-
-def gradient_descent(x,y, learning_rate):
-    d = gradient(x, y)
-    x = x - learning_rate * d[0]
-    y = y - learning_rate * d[1]
-
-    return x, y
-
-def find_minimum(initial_x, initial_y, learning_rate, num_iterations, tolerance):
-    x = initial_x
-    y = initial_y
-    learning_rate = learning_rate
-    x_history = [x]
-    y_history = [y]
-
-    for i in range(num_iterations):
-        x,y = gradient_descent(x,y,learning_rate)
-        x_history.append(x)
-        y_history.append(y)
-        if i > 0 and abs(f(x_history[i], y_history[i]) - f(x_history[i-1], y_history[i-1])) <= tolerance:
-            break
-            
-    return x, x_history, y, y_history
-
 # Set initial values
 initial_x = 0
 initial_y = -1
 learning_rate = 0.1
 num_iterations = 1000
-tolerance = 1e-100
+tolerance = 1e-1000
 
-# Run gradient descent
-x, x_history, y, y_history = find_minimum(initial_x, initial_y, learning_rate, num_iterations, tolerance)
+
+x, x_history, y, y_history = gradient_descent(initial_x, initial_y, learning_rate, num_iterations, tolerance)
+
 
 # Print the final result
 print("Local minimum: {:.6f}".format(f(x, y)))
 print("Final Point: x = {:.6f}, y = {:.6f}".format(x, y))
+print(len(x_history))
 
 
 # Create a range of x and y values for plotting
-x_vals = np.linspace(-2.0, 2.0, 100)
-y_vals = np.linspace(-2.5, 2.5, 100)
+x_vals = np.linspace(-2.0, 2.5, 100)
+y_vals = np.linspace(-2.0, 2.0, 100)
 X, Y = np.meshgrid(x_vals, y_vals)
 Z = f(X, Y)
 
