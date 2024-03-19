3f9c9a0513542d040c54032b773cc16880373c47
mik00laj <mikolaj.bania@gmail.com>
2024-03-19 01:35:43 +0100

    koniec cw 2

--- Task 2/WSI - task 2/main.py
+++ Task 2/WSI - task 2/main.py
@@ -4,33 +4,33 @@ import copy
 from matplotlib import pyplot as plt
 from typing import Tuple
 
-MU = 1
-LAMBDA = 1
+MU = 128
+LAMBDA = 512
 SIGMA = 0.1
 ITERATIONS = 200
 
 def f(x, y):
-    return (9 * x * y) / np.exp(x ** 2 + 0.5 * x + y ** 2)
+    return -(9 * x * y) / np.exp(x ** 2 + 0.5 * x + y ** 2)
+def neg_f(x, y):
+    return -(9 * x * y) / np.exp(x ** 2 + 0.5 * x + y ** 2)
 
-def evolution_es(f, MU: int, LAMBDA: int, SIGMA: float, ITERATIONS: int) -> Tuple[np.ndarray, float, list]:
+def evolution_es_min(f, MU: int, LAMBDA: int, SIGMA: float, ITERATIONS: int) -> Tuple[np.ndarray, float, list]:
     population = list()
     x_history = []
     y_history = []
     f_history = []
 
     for i in range(MU):
-        x = np.random.uniform(-3.0, 3.0)
-        y = np.random.uniform(-3.0, 3.0)
+        x = np.random.uniform(-20.0, 20.0)
+        y = np.random.uniform(-200.0, 20.0)
         population.append(([x, y], np.array([SIGMA] * 2)))
-        x_history.append(x)
-        y_history.append(y)
 
     score = [f(x, y) for [x, y], _ in population]
     best_ever = min(zip(population, score), key=lambda a: a[1])
 
     min_ypoints = [0.0] * ITERATIONS
     for epoch in range(ITERATIONS):
-        parents = [tournament_selection(population) for _ in range(LAMBDA)]
+        parents = [tournament_selection_min(population) for _ in range(LAMBDA)]
         children = [crossover(f, parents) for _ in range(LAMBDA)]
         children = [mutation(f, e) for e in children]
 
@@ -52,7 +52,49 @@ def evolution_es(f, MU: int, LAMBDA: int, SIGMA: float, ITERATIONS: int) -> Tupl
 
     return best_ever[0][0], f(*best_ever[0][0]), population, f_history, x_history, y_history
 
-def tournament_selection(population: list) -> Tuple[np.ndarray, np.ndarray]:
+def evolution_es_max(f, MU: int, LAMBDA: int, SIGMA: float, ITERATIONS: int) -> Tuple[np.ndarray, float, list]:
+    population = list()
+    x_history = []
+    y_history = []
+    f_history = []
+
+    for i in range(MU):
+        x = np.random.uniform(-20.0, 20.0)
+        y = np.random.uniform(-20.0, 20.0)
+        population.append(([x, y], np.array([SIGMA] * 2)))
+
+    score = [f(x, y) for [x, y], _ in population]
+    best_ever = max(zip(population, score), key=lambda a: a[1])
+
+    min_ypoints = [0.0] * ITERATIONS
+    for epoch in range(ITERATIONS):
+        parents = [tournament_selection_max(population) for _ in range(LAMBDA)]
+        children = [crossover(f, parents) for _ in range(LAMBDA)]
+        children = [mutation(f, e) for e in children]
+
+        best_epoch = max(zip(children, score), key=lambda a: a[1])
+        if best_epoch[1] > best_ever[1]:
+            best_ever = copy.deepcopy(best_epoch)
+
+        population += children
+        score = [f(x, y) for [x, y], _ in population]
+
+        population = [indiv for _, indiv in sorted(zip(score, population), key=lambda pair: pair[0], reverse=True)]  # Sortowanie malejąco
+        population = population[:MU]
+
+        min_ypoints[epoch] = f(*population[0][0])
+        f_history.append(min_ypoints[epoch])
+        x_history.append(population[0][0][0])
+        y_history.append(population[0][0][1])
+        print("Pokolenie: ", epoch, "f(x,y)= ", min_ypoints[epoch])
+
+    return best_ever[0][0], f(*best_ever[0][0]), population, f_history, x_history, y_history
+
+def tournament_selection_max(population: list) -> Tuple[np.ndarray, np.ndarray]:
+    tournament_size = 2
+    tournament_contestants = random.choices(population, k=tournament_size)
+    return max(tournament_contestants, key=lambda x: f(*x[0]))
+def tournament_selection_min(population: list) -> Tuple[np.ndarray, np.ndarray]:
     tournament_size = 2
     tournament_contestants = random.choices(population, k=tournament_size)
     return min(tournament_contestants, key=lambda x: f(*x[0]))
@@ -85,7 +127,10 @@ def mutation(f, parent: Tuple[np.ndarray, np.ndarray]):
         x[i] = x[i] + SIGMA[i] * random.normalvariate(0.0, 1.0)
     return x, SIGMA
 
-best_solution, best_fitness, population, f_history, x_history, y_history = evolution_es(f, MU, LAMBDA, SIGMA, ITERATIONS)
+# FIND MINIMUM
+best_solution, best_fitness, population, f_history, x_history, y_history = evolution_es_min(f, MU, LAMBDA, SIGMA, ITERATIONS)
+# FIND MAXIMUM
+# best_solution, best_fitness, population, f_history, x_history, y_history = evolution_es_max(neg_f, MU, LAMBDA, SIGMA, ITERATIONS)
 
 print("Best solution:", best_solution)
 print("Objective function value at the best solution:", best_fitness)
